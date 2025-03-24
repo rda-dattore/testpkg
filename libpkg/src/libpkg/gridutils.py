fc638e64af44bff34cc952dd0c513e203d518c93
rda-dattore <dattore@ucar.edu>
2025-03-24 16:28:16 -0600

    change return type

--- libpkg/src/libpkg/gridutils.py
+++ libpkg/src/libpkg/gridutils.py
@@ -18,16 +18,13 @@ def decode_elongitude(lons):
 
 
 def spatial_domain_from_grid_definition(gdef, **kwargs):
-    wlon = 999.
-    slat = 999.
-    elon = -999.
-    nlon = -999.
+    domain = {'wlon': None, 'slat': None, 'elon': None, 'nlat': None}
     parts = gdef[1].split(":")
     if re.compile("^(latLon|gaussLatLon|mercator)(Cell){0,1}$").match(gdef[0]):
         start_lat = decode_latitude(parts[2])
         end_lat = decode_latitude(parts[4])
-        slat = min(start_lat, end_lat)
-        nlat = max(start_lat, end_lat)
+        domain['slat'] = min(start_lat, end_lat)
+        domain['nlat'] = max(start_lat, end_lat)
         start_elon = decode_elongitude(parts[3])
         end_elon = decode_elongitude(parts[5])
         xspace = float(parts[0])
@@ -49,7 +46,7 @@ def spatial_domain_from_grid_definition(gdef, **kwargs):
             start_elon += 360.
             scans_east = False
         else:
-            return (wlon, slat, elon, nlat)
+            return domain
 
         # adjust global grids where boundary is not repeated
         is_global_lon = False
@@ -64,38 +61,43 @@ def spatial_domain_from_grid_definition(gdef, **kwargs):
                     if abs(abs(end_elon - xres - start_elon) - 360.) < 0.001:
                         is_global_lon = True
 
-        if abs(abs(nlat - slat + float(parts[7])) - 180.) < 0.001:
-            slat = -90.
-            nlat = 90.
+        if (abs(abs(domain['nlat'] - domain['slat'] +
+                float(parts[7])) - 180.) < 0.001):
+            domain['slat'] = -90.
+            domain['nlat'] = 90.
 
         if is_global_lon:
             if 'centerOn' in kwargs:
                 if kwargs['centerOn'] == "dateLine":
-                    wlon = 0.
-                    elon = 360.
+                    domain['wlon'] = 0.
+                    domain['elon'] = 360.
                 elif kwargs['centerOn'] == "primeMeridian":
-                    wlon = -180.
-                    elon = 180.
+                    domain['wlon'] = -180.
+                    domain['elon'] = 180.
 
         elif 'centerOn' in kwargs and kwargs['centerOn'] == "dateLine":
             if scans_east:
-                wlon = start_elon
-                elon = end_elon
+                domain['wlon'] = start_elon
+                domain['elon'] = end_elon
             else:
-                wlon = end_elon
-                elon = start_elon
+                domain['wlon'] = end_elon
+                domain['elon'] = start_elon
 
         elif 'centerOn' in kwargs and kwargs['centerOn'] == "primeMeridianl":
             if scans_east:
-                wlon = (start_elon - 360.) if start_elon >= 180. else start_elon
-                elon = (end_elon - 360.) if end_elon > 180. else end_elon
+                domain['wlon'] = ((start_elon - 360.) if start_elon >= 180.
+                                  else start_elon)
+                domain['elon'] = ((end_elon - 360.) if end_elon > 180. else
+                                  end_elon)
             else:
-                wlon = (end_elon - 360.) if end_elon >= 180. else end_elon
-                elon = (start_elon - 360.) if start_elon > 180. else start_elon
+                domain['wlon'] = ((end_elon - 360.) if end_elon >= 180. else
+                                  end_elon)
+                domain['elon'] = ((start_elon - 360.) if start_elon > 180.
+                                  else start_elon)
 
     elif gdef[0].find("lambertConformal") == 0:
         pass
     elif gdef[0].find("polarStereographic") == 0:
         pass
 
-    return (wlon, slat, elon, nlat)
+    return domain
