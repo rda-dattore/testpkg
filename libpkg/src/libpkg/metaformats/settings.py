3bbb4b34a233919e1d39ae5db94c693e72f954af
rda-dattore <dattore@ucar.edu>
2025-04-10 17:11:10 -0600

    update the settings

--- libpkg/src/libpkg/metaformats/settings.py
+++ libpkg/src/libpkg/metaformats/settings.py
@@ -1,5 +1,24 @@
-PUBLISHER = "UCAR/NCAR - Research Data Archive"
-
-GDEX_DATASETS_URL = "https://rda.ucar.edu/datasets"
+ARCHIVE = {
+    'name': "NSF NCAR Research Data Archive",
+    'pub_name': "UCAR/NCAR - Research Data Archive",
+    'description': ("The Research Data Archive (RDA), managed by the Data "
+                    "Engineering and Curation Section (DECS) of the "
+                    "Computational and Information Systems Laboratory (CISL) "
+                    "at NSF NCAR, contains a large and diverse collection of "
+                    "meteorological and oceanographic observations, "
+                    "operational and reanalysis model outputs, and remote "
+                    "sensing datasets to support atmospheric and geosciences "
+                    "research, along with ancillary datasets, such as "
+                    "topography/bathymetry, vegetation, and land use."),
+    'url': "https://rda.ucar.edu",
+    'datasets_url': "https://rda.ucar.edu/datasets",
+}
 
 DOI_DOMAIN = "https://doi.org"
+
+GCMD = {
+    'org_name': "NASA Global Change Master Directory",
+    'title': ("U.S. National Aeronautics and Space Administration Global "
+              "Change Master Directory"),
+    'alternate_title': "GCMD",
+}
