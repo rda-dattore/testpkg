68d81b145509b8b62c01b63f737cc06e7601cadf
rda-dattore <dattore@ucar.edu>
2025-04-18 13:31:16 -0600

    start building export code

--- libpkg/src/libpkg/metaformats/gcmd_dif.py
+++ libpkg/src/libpkg/metaformats/gcmd_dif.py
@@ -1,2 +1,28 @@
+import psycopg2
+
+from lxml import etree
+
+
 def export(dsid, metadb_settings):
-    pass
+    try:
+        mconn = psycopg2.connect(**metadb_settings)
+        mcursor = mconn.cursor()
+    except psycopg2.Error as err:
+        raise RuntimeError("metadata database connection error: '{}'"
+                           .format(err))
+
+    warnings = []
+    try:
+        nsmap = {
+            None: "http://gcmd.gsfc.nasa.gov/Aboutus/xml/dif/",
+            'xsi': "http://www.w3.org/2001/XMLSchema-instance",
+        }
+        schema_loc = etree.QName(nsmap['xsi'], "schemaLocation")
+        xsd = nsmap[None] + "dif_v9.7.1.xsd"
+        root = etree.Element("DIF", {schema_loc: " ".join([nsmap[None], xsd])},
+                             nsmap=nsmap)
+    finally:
+        mconn.close()
+
+    return (etree.tostring(root, pretty_print=True).decode("utf-8"),
+            "\n".join(warnings))
