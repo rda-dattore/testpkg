67d7cee8b9167d3758fcde65bc68764c55a2527e
rda-dattore <dattore@ucar.edu>
2025-07-07 06:24:41 -0600

    add author title

--- libpkg/src/libpkg/metaformats/iso_19139.py
+++ libpkg/src/libpkg/metaformats/iso_19139.py
@@ -210,10 +210,10 @@ def add_di_citation(root, nsmap, nil_reason, dsid, authors, cursor, title,
             author_list.append({
                 'type': "P",
                 'name': (author.get("lname") + ", " + author.get("fname") + " "
-                         + author.get("mname")).strip()})
-                #'name': ((author.get("fname") + " " +
-                #          author.get("mname")).strip() + " " +
-                #         author.get("lname"))})
+                         + author.get("mname")).strip(),
+                'title': ((author.get("fname") + " " +
+                          author.get("mname")).strip() + " " +
+                          author.get("lname"))})
             orcid_id = author.get("orcid_id")
             if orcid_id is not None:
                 author_list[-1].update({'orcid_id': orcid_id})
