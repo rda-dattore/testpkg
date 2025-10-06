ff52151f9f5c38dd7717b364abf9cf63ec5fafd1
rda-dattore <dattore@ucar.edu>
2025-10-06 14:58:01 -0600

    initial commit

---
+++ dset_waf/src/dset_waf/dset_waf.py
@@ -0,0 +1,183 @@
+import json
+import psycopg2
+import sys
+
+from libpkg.metaformats import iso_19139
+from libpkg.strutils import strand
+from lxml import etree
+
+
+REPO_HEAD = "/data/dset_waf/git-repos"
+
+GIT_REPOS = [
+    "dset-web-accessible-folder-dev/rda",
+    "dash-rda-prod/RDA-Datasets",
+]
+
+
+def do_push(args):
+    if len(args) < 3:
+        print("Error: missing argument(s) for PUSH")
+        sys.exit(1)
+
+    wdb_config = json.loads(args[-1])
+    del args[-1]
+    mdb_config = json.loads(args[-1])
+    del args[-1]
+    push_list = []
+    if len(args) > 1:
+        for arg in args:
+            if arg[0] == 'd':
+                push_list.append(arg)
+            else:
+                print("Error: invalid dataset ID '{}'".format(arg))
+                sys.exit(1)
+
+    elif args[0][0] == 'd':
+        push_list.append(args[0])
+
+    try:
+        mconn = psycopg2.connect(**mdb_config)
+        mcursor = mconn.cursor()
+        if len(push_list) == 0:
+            if args[0] == "all":
+                mcursor.execute((
+                        "select dsid from search.datasets where type in "
+                        "('P', 'H')"))
+            elif args[0] == "queued-only":
+                mcursor.execute((
+                        "select w.dsid from metautil.dset_waf as w left join "
+                        "search.datasets as d on d.dsid = w.dsid where d.type "
+                        "in ('P', 'H') and w.uflag = ''"))
+            else:
+                print("Error: invalid DSID_LIST")
+                sys.exit(1)
+
+            res = mcursor.fetchall()
+            push_list = [e[0] for e in res]
+
+        if len(push_list) == 0:
+            print("No matching datasets found.")
+            sys.exit(1)
+
+        uflag = strand(10)
+        mcursor.execute("update metautil.dset_waf set uflag = %s", (uflag, ))
+        mconn.commit()
+        wconn = psycopg2.connect(**wdb_config)
+        wcursor = wconn.cursor()
+        for dsid in push_list:
+            print(dsid)
+            iso_rec = iso_19139.export(dsid, mdb_config, wdb_config)
+            # validate the ISO record
+            root = etree.fromstring(iso_rec).find(".")
+            print("ROOT=" + str(root))
+            xml_schema_doc = etree.parse("/data/dset_waf/schemas/iso/iso19139.xsd")
+            print("XML_SCHEMA_DOC=" + str(xml_schema_doc))
+            #xml_schema = etree.XMLSchema("/data/dset_waf/schemas/iso/iso19139.xsd")
+            #print("XML_SCHEMA=" + str(xml_schema))
+            sys.exit(1)
+
+    except Exception as err:
+        print("Database connection error '{}'".format(err))
+    finally:
+        if 'mconn' in locals():
+            mconn.close()
+
+        if 'wconn' in locals():
+            wconn.close()
+
+
+def do_delete(args):
+    print("do_delete")
+
+
+def do_dbreset(args):
+    if len(args) < 1:
+        print("Error: missing DB configuration")
+        sys.exit(1)
+
+    db_config = json.loads(args[0])
+    try:
+        conn = psycopg2.connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("select distinct dsid from metautil.dset_waf")
+        dsids = cursor.fetchall()
+        try:
+            cursor.execute("delete from metautil.dset_waf")
+            conn.commit()
+            for dsid in dsids:
+                cursor.execute("insert into metautil.dset_waf values (%s, '')",
+                               (dsid, ))
+                conn.commit()
+        except Exception as err:
+            print(("Error while trying to clear a failed push: '{}', list: "
+                   "'{}'").format(err, ", ".join(dsids)))
+
+    except Exception as err:
+        print("Error while trying to fix a failed push: '{}'".format(err))
+    finally:
+        if 'conn' in locals():
+            conn.close()
+
+
+ACTIONS = {
+    'PUSH': {
+        'description': "add/update dataset(s)",
+        'callable': do_push,
+    },
+    'DELETE': {
+        'description': "remove dataset(s)",
+        'callable': do_delete,
+    },
+    'DBRESET': {
+        'description': "reset the database after a failed push",
+        'callable': do_dbreset,
+    },
+}
+
+
+def print_usage_and_exit():
+    print(("usage: dset_waf PUSH DSID_LIST META_DBCONFIG WAGTAIL_CONFIG"))
+    print("  or:  dset_waf DELETE DSID_LIST META_DBCONFIG")
+    print("  or:  dset_waf DBRESET META_DBCONFIG")
+    print("")
+    print("valid actions:")
+    for key, val in ACTIONS.items():
+        print("    " + " - ".join([key, val['description']]))
+
+    print("")
+    print("valid DSID_LIST specifications:")
+    print(("    'dnnnnnn ...':          specify one or more individual "
+           "dataset IDs"))
+    print(("    'all':                  identify all public datasets"))
+    print(("    'queued-only' (PUSH):   only push datasets that are queued in "
+           "the database"))
+    print(("    'non-public' (DELETE):  identify and delete just "
+           "non-public datasets"))
+    print("")
+    print(("META_DBCONFIG   dictionary of metadata DB configuration "
+           "parameters as:"))
+    print(('    {"user": "<U>", "password": "<P>", "host": "<H>", "dbname": '
+           '"<D>"}'))
+    print(("         where U = username, P = password, H = host name, D = "
+           "database name"))
+    print("")
+    print(("WAGTAIL_CONFIG  dictionary of wagtail DB configuration "
+           "parameters (like"))
+    print("                META_DBCONFIG)")
+    sys.exit(1)
+
+
+def main():
+    if len(sys.argv) == 1 or sys.argv[1] == "--help":
+        print_usage_and_exit()
+
+    if sys.argv[1] not in ACTIONS.keys():
+        print("Error: invalid action - must be one of: " +
+              ", ".join([("'" + key + "'") for key in ACTIONS.keys()]))
+        sys.exit(1)
+
+    if 'callable' not in ACTIONS[sys.argv[1]]:
+        raise KeyError("no callable found for this action")
+
+    ACTIONS[sys.argv[1]]['callable'](sys.argv[2:])
