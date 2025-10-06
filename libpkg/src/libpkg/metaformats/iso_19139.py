0785a623070378d6d0c9f1f9d646a5d3c4327ea5
rda-dattore <dattore@ucar.edu>
2025-10-06 16:32:28 -0600

    fix typo

--- libpkg/src/libpkg/metaformats/iso_19139.py
+++ libpkg/src/libpkg/metaformats/iso_19139.py
@@ -863,7 +863,7 @@ def add_distribution_info(root, nsmap, dsid, size):
     ci_onlineresource = etree.SubElement(
             etree.SubElement(
                     md_digitaltransferoptions,
-                    "{" + nsmap['gmd'] + "}online"),
+                    "{" + nsmap['gmd'] + "}onLine"),
             "{" + nsmap['gmd'] + "}CI_OnlineResource")
     durl = os.path.join("https://", settings.ARCHIVE['domain'],
                         settings.ARCHIVE['datasets_path'])
