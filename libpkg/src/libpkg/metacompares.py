02e99d56d4a0d09601764a9521b4209a53a1aabb
rda-dattore <dattore@ucar.edu>
2026-05-20 16:25:13 -0600

    increment version

---
+++ libpkg/src/libpkg/metacompares.py
@@ -0,0 +1,59 @@
+def time_range_parts(t):
+    tr_parts = [0, "", ""]
+    if t.find("(initial+") > 0:
+        parts = t.split("+")
+        tparts = parts[0].split()
+        tr_parts[2] = tparts[-2]
+        tr_parts[0] = int(parts[1][0:parts[1].find(" ")])
+        tr_parts[1] = parts[2][:-1]
+    elif t.find("-") > 0:
+        if t.find("Forecast") > 0:
+            tr_parts[0] = -1
+        else:
+            tr_parts[0] = 0
+
+        idx = t.find("-")
+        tr_parts[1] = t[0:idx]
+        tr_parts[2] = t[idx+1:]
+        idx = tr_parts[1].find(" ")
+        if idx > 0:
+            tr_parts[2] = tr_parts[2][idx+1]
+
+    else:
+        tr_parts[0] = 0
+        tr_parts[1] = t
+
+    return tr_parts
+
+
+def compare_time_ranges(t1, t2):
+    if t1[0:6] == "Analys":
+        return -1
+
+    if t2[0:6] == "Analys":
+        return 1
+
+    t1_start, t1_end, t1_label = time_range_parts(t1)
+    t2_start, t2_end, t2_label = time_range_parts(t2)
+    if len(t1_end) < len(t2_end):
+        t1_end = (len(t2_end) - len(t1_end)) * '0' + t1_end
+
+    if len(t2_end) < len(t1_end):
+        t2_end = (len(t1_end) - len(t2_end)) * '0' + t2_end
+
+    if t1_end < t2_end:
+        return -1
+
+    if t2_end < t1_end:
+        return 1
+
+    if t1_start < t2_start:
+        return -1
+
+    if t2_start < t1_start:
+        return 1
+
+    if t1_label < t2_label:
+        return -1
+
+    return 1
