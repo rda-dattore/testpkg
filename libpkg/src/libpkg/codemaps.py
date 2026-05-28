8559cd643bd463496bf8fa5889d38506d6207fb4
rda-dattore <dattore@ucar.edu>
2026-05-28 13:06:59 -0600

    initial commit

---
+++ libpkg/src/libpkg/codemaps.py
@@ -0,0 +1,58 @@
+import os
+
+from lxml import etree
+
+
+def decode_level(data_format, map, type, value, mapset):
+    mapset_key = ".".join([data_format, map, "xml"])
+    if mapset_key not in mapset:
+        mapset[mapset_key] = etree.parse(os.path.join(
+                "/data/web/metadata/LevelTables", mapset_key)).getroot()
+
+    types = type.split("-")
+    if len(types) == 1:
+        tree = mapset[mapset_key].find(f"./level[@code='{type}']")
+        if tree is None:
+            tree = mapset[mapset_key].find(f"./layer[@code='{type}']")
+
+        if tree is None:
+            return ""
+
+        desc = tree.find("./description")
+        if desc is None:
+            return ""
+
+        units = tree.find("./units")
+        if units is None or units.text is None:
+            if value == "0":
+                return desc.text
+
+            return "".join([desc.text, ": ", value])
+
+        return "".join([desc.text, ": ", value, " ", units.text])
+
+    tree1 = mapset[mapset_key].find(f"./level[@code='{types[0]}']")
+    tree2 = mapset[mapset_key].find(f"./level[@code='{types[1]}']")
+    if tree1 is None or tree2 is None:
+        return ""
+
+    desc1 = tree1.find("./description")
+    desc2 = tree2.find("./description")
+    if desc1 is None or desc2 is None:
+        return ""
+
+    vals = value.split(",")
+    units1 = tree1.find("./units")
+    if units1 is not None:
+        vals[0] = "".join([vals[0], " ", units1.text])
+
+    units2 = tree2.find("./units")
+    if units2 is not None:
+        vals[1] = "".join([vals[1], " ", units2.text])
+
+    if desc1.text == desc2.text:
+        return "".join(["Layer between two '", desc1.text, "': ", vals[0],
+                        ", ", vals[1]])
+
+    return "".join(["Layer between '", desc1.text, "': ", vals[0], " and '",
+                    desc2.text, "': ", vals[1]])
